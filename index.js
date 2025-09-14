ea0a8a6a81028e86f55561bc05182a14e7bfb386
diegomolloai <diegomolloai@gmail.com>
2025-09-13 22:16:57 -0300

    fix: Update image paths and favicon
    Correctly points the logo image to its root path and updates the favicon reference to be relative, improving asset loading and organization.

--- index.js
+++ index.js
@@ -640,7 +640,7 @@ const LoginScreen = ({ onLogin }) => {
     return React.createElement(Page, null,
         React.createElement("div", { className: "relative w-full flex flex-col items-center" },
             React.createElement("img", {
-                src: "public/assets/image/Impostor_Futbolero_Logo_SF.png",
+                src: "Impostor_Futbolero_Logo_SF.png",
                 alt: "El Impostor Futbolero Logo",
                 className: "absolute inset-0 w-[48rem] h-[48rem] m-auto opacity-50 -z-10"
             }),
