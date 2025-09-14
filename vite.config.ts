ca9603f368d8a53b2f0a72488cc846e394047fbc
diegomolloai <diegomolloai@gmail.com>
2025-09-14 05:16:17 +0000

    revert: Eliminar base para GitHub Pages en vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 
+
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/Impostor2/', // Necesario para GitHub Pages
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
